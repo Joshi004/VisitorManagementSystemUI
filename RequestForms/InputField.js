9ce80ba8ac9a76e83baf060dd3d32960a8a2f084
Joshi004 <thinkjoshi@gmail.com>
2019-03-05 02:24:20 +0530

    Label text visible only when input field is focused

--- RequestForms/InputField.js
+++ RequestForms/InputField.js
@@ -21,15 +21,15 @@ export default class InputField extends Component {
   render() {
       return (
         <View style={styles.inputField}>
-        <Text style={styles.label}>{this.props.placeholder}</Text>
+        <Text style={this.state.isFocused ? styles.label: ''}>{this.state.isFocused ? this.props.placeholder : ''}</Text>
             <TextInput 
                 placeholder={this.props.placeholder}
                 selectionColor={this.props.selectionColor}
                 keyboardType={this.props.keyboardType}
-                underlineColorAndroid={this.state.isFocused ? 'blue' : 'black'}
+                value={this.state[this.props.fieldName]}
+                underlineColorAndroid={this.state.isFocused ? 'rgb(137, 182, 246)' : 'rgb(155, 155, 155)'}
                 style={styles.inputField}
                 onChangeText={(text) => this.setState({ text })}
-                value={this.state[this.props.fieldName]}
                 onFocus={this.handleFocus}
                 onBlur={this.handleBlur}
               />
